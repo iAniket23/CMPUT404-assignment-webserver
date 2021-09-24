e133904de995e1dba4b90a021f0de54d530e8f21
Aniket Mishra <ianiket23@gmail.com>
2021-09-24 14:46:49 -0600

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ from os.path import isdir
 from os.path import isfile
 from datetime import datetime, timezone
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos    
+# Copyright 2021 Aniket Mishra
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
